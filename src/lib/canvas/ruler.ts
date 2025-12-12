ddbc7da993f01aafa1265cf67a5e4eb83cfd1ddf
Mahmoud Omiesh <mahmoudomiesh2005@gmail.com>
2025-12-12 15:58:45 +0200

    feat: veritcal ruler

--- src/lib/canvas/ruler.ts
+++ src/lib/canvas/ruler.ts
@@ -67,12 +67,60 @@ export class Ruler {
 				offscreenCtx.fillText(String(coord), screenPos.x + 4, 0.55 * this.config.size);
 			} else {
 				const isMedium = Math.abs(coord % (majorStep / 2)) < 0.0001;
-				const y = (isMedium ? 0.65 : 0.75) * this.config.size;
+				const y = (isMedium ? 0.6 : 0.75) * this.config.size;
 				offscreenCtx.fillRect(screenPos.x, y, 1, this.config.size - y);
 			}
 		}
 
-		offscreenCtx.fillRect(0, 17, width, 1);
+		offscreenCtx.fillRect(0, this.config.size, width, 1);
+	}
+
+	private renderVeritcalRuler(height: number) {
+		const offscreenCtx = this.offscreenCanvasContext;
+		const topLeft = this.viewport.screenToViewport({
+			x: 0,
+			y: 0
+		});
+		const bottomLeft = this.viewport.screenToViewport({
+			x: 0,
+			y: height
+		});
+		const scale = this.viewport.getScale();
+
+		const rawMajorStep = this.config.pixelsPerMajorStep / scale;
+		const majorStep =
+			NICE_STEPS.find((x) => x >= rawMajorStep) ?? NICE_STEPS[NICE_STEPS.length - 1];
+
+		const minorStep = majorStep / this.config.minorSteps;
+
+		const beginning = Math.ceil(topLeft.y / minorStep) * minorStep;
+
+		offscreenCtx.font = '12px monospace';
+		offscreenCtx.fillStyle = '#e2e8f0';
+
+		for (let coord = beginning; coord <= bottomLeft.y; coord += minorStep) {
+			const screenPos = this.viewport.viewportToScreen({ x: 0, y: coord });
+
+			const isMajor = Math.abs(coord % majorStep) < 0.0001;
+
+			if (isMajor) {
+				offscreenCtx.fillRect(0, screenPos.y, this.config.size, 1);
+				const textX = 0.15 * this.config.size;
+				const textY = screenPos.y + 4;
+
+				offscreenCtx.save();
+				offscreenCtx.translate(textX, textY);
+				offscreenCtx.rotate(Math.PI / 2);
+				offscreenCtx.fillText(String(coord), 0, 0);
+				offscreenCtx.restore();
+			} else {
+				const isMedium = Math.abs(coord % (majorStep / 2)) < 0.0001;
+				const x = (isMedium ? 0.6 : 0.75) * this.config.size;
+				offscreenCtx.fillRect(x, screenPos.y, this.config.size - x, 1);
+			}
+		}
+
+		offscreenCtx.fillRect(this.config.size, 0, 1, height);
 	}
 
 	getImageBitmap({ width, height }: { width: number; height: number }) {
@@ -80,6 +128,7 @@ export class Ruler {
 		this.offscreenCanvas.height = height;
 
 		this.renderHorizontalRuler(width);
+		this.renderVeritcalRuler(height);
 
 		return this.offscreenCanvas.transferToImageBitmap();
 	}
